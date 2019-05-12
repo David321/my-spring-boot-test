a5b14e694cb6977d544457e066e48792056e41c5
David321 <2287070944@qq.com>
2019-05-12 11:32:00 +0800

    新增IndexController

---
+++ src/main/java/com/davidcorp/myspringboottest/IndexController.java
@@ -0,0 +1,13 @@
+package com.davidcorp.myspringboottest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+
+    @RequestMapping(value="index")
+    public String index(){
+        return "这是首页";
+    }
+}
