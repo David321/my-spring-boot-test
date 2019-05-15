d9c703e9c58a032ab4804e1af3b563379efa06a0
David321 <2287070944@qq.com>
2019-05-15 17:14:09 +0800

    多线程

--- src/main/java/com/davidcorp/myspringboottest/MultiThreadingTest.java
+++
@@ -1,7 +0,0 @@
-package com.davidcorp.myspringboottest;
-
-public class MultiThreadingTest {
-    public static void main(String[] args) {
-        Thread thread = new Thread();
-    }
-}
---
+++ src/main/java/com/davidcorp/myspringboottest/ThreadTest2.java
@@ -0,0 +1,17 @@
+package com.davidcorp.myspringboottest;
+
+public class ThreadTest2 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 100; i++) {
+            System.out.println(Thread.currentThread().getName() + " " + i);
+            if (i == 30) {
+                MyRunnable myRunnable = new MyRunnable();
+                Thread thread1 = new Thread(myRunnable);
+                Thread thread2 = new Thread(myRunnable);
+                // thread1和thread2才是真正的线程实例
+                thread1.start();
+                thread2.start();
+            }
+        }
+    }
+}
