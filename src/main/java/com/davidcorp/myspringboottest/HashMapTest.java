0409bdb2bd83df02fa7f6b98ad6cdd78a2dfc13d
David321 <2287070944@qq.com>
2019-05-17 11:22:16 +0800

    测试HashMap和HashTable

---
+++ src/main/java/com/davidcorp/myspringboottest/HashMapTest.java
@@ -0,0 +1,20 @@
+package com.davidcorp.myspringboottest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HashMapTest {
+
+    public static void main(String[] args) {
+        HashMap<String, String> hm = new HashMap<>();
+        hm.put(null,"david");
+        hm.put(null,"obama");// HashMap可以接受null值作为key；且，put相同的key，会覆盖之前的
+        hm.put("zhangsan", null);// hashmap可以接受null值作为value
+        hm.put("addr","changsha");
+        System.out.println("-----------"+hm.get("zhangsan"));
+        // 遍历hashmap
+        for (Map.Entry<String, String> entry : hm.entrySet()) {
+            System.out.println("key:"+entry.getKey()+"，value:"+entry.getValue());
+        }
+    }
+}
