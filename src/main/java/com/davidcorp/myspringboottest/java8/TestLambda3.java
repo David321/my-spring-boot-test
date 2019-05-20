22f4979afc8f8e48947442a4ee18c7052ab31036
David321 <2287070944@qq.com>
2019-05-20 16:41:03 +0800

    Java8新特性

---
+++ src/main/java/com/davidcorp/myspringboottest/java8/TestLambda3.java
@@ -0,0 +1,20 @@
+package com.davidcorp.myspringboottest.java8;
+
+import java.util.Comparator;
+import java.util.function.Consumer;
+
+public class TestLambda3 {
+    // Java8四大内置函数式接口
+    // consumer supplier function predicate
+
+    // 有传入参数，无返回参数
+    public static void test1(String s, Consumer<String> consumer) {
+        consumer.accept(s);
+    }
+
+    public static void main(String[] args) {
+        Comparator<Integer> com = Integer::compareTo;
+        Integer result = com.compare(1,2);
+        System.out.println(result);
+    }
+}
