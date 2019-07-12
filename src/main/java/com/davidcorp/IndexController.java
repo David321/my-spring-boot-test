6ea18bc33d853f26dd510a436d23398085e6ac72
David321 <2287070944@qq.com>
2019-07-12 22:38:34 +0800

    测试快捷键添加方法注释

--- src/main/java/com/davidcorp/IndexController.java
+++ src/main/java/com/davidcorp/IndexController.java
@@ -6,6 +6,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class IndexController {
 
+    /*
+     * @Description
+     * @Author DavidPeng
+     * @Date 2019/7/12 22:37
+     **/
     @RequestMapping(value="index")
     public String index(){
         return "这是首页";
