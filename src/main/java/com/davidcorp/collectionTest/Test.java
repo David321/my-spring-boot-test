d3230e9307d6dbe51966c970b6fb30439808116e
David321 <2287070944@qq.com>
2020-02-29 15:29:05 +0800

    添加说明文件

--- src/main/java/com/davidcorp/collectionTest/Test.java
+++ src/main/java/com/davidcorp/collectionTest/Test.java
@@ -5,17 +5,27 @@ import java.util.*;
 public class Test {
 
     public static void main(String[] args) {
-        // 集合相关接口
+        // 集合类的顶层接口
         Collection collection;
-        List list;
-        Set set;
-        Map map;
 
-        // 相关实现类
-        HashMap hashMap;
-        TreeMap treeMap;
+        // List接口
+        List list;
+        // 主要实现类
+        ArrayList arrayList;
+        LinkedList linkedList;
+
+        // Set接口
+        Set set;
+        // 主要实现类
         HashSet hashSet;
         TreeSet treeSet;
+
+        // Map接口
+        Map map;
+        // 主要实现类
+        HashMap hashMap;
+        TreeMap treeMap;
+
 
         // 两个用来做排序的接口
         Comparator comparator;
